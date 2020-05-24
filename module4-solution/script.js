b3bf6cf4c66798d3edc42513fe67ab8fbd7f664b
sreepurampriya <sreepurams.18.beis@acharya.ac.in>
2020-05-24 01:40:10 -0700

    first modification.

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -14,7 +14,7 @@ Good Bye Jim
 */
 
 (function() {
-    var names = ["Yaakov", "BTS", "Sunny", "namjoon","jin", "yoongi", "jhope", "jimin", "taehyung", "jungkook", "v"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
